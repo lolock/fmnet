2b84d43aacda3d6fa18331312a28879854bbf496
JaC <jac@JaCdeMac-mini.local>
2025-07-06 20:08:39 +0800

    troubleshooting3

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -7,15 +7,6 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  * Home Layout: app/(home)/layout.tsx
  * Docs Layout: app/docs/layout.tsx
  */
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
-
-/**
- * Shared layout configurations
- *
- * you can customise layouts individually from:
- * Home Layout: app/(home)/layout.tsx
- * Docs Layout: app/docs/layout.tsx
- */
 export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
